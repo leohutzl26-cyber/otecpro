3321edd1f2f2a49025b47b2684c923a0babaccce
Facilita Admin <admin@facilita.com>
2026-05-03 21:43:22 -0400

    fix: remover dependencias no instaladas de react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './components/theme-provider'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="light" storageKey="otecpro-theme">
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="light" storageKey="otecpro-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
